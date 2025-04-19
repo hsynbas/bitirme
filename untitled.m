434e8b1923394b936991ee974592df56b628bcb8
okuryigit <okuryigit02@gmail.com>
2025-04-19 21:25:36 +0300

    yigit deneme

--- untitled.m
+++ untitled.m
@@ -1,18 +1,23 @@
 clc; clear;
-%Infinite Batt - Best Eff
-% Parametreler
-q = 0.5;
-R = 4;
-n_values = [1, 2, 4, 6, 8];
-sim_time = 5000;
-num_trials = 1000;
 
-AoI_packet = zeros(num_trials, length(n_values));
 
-% Simülasyon
+q = 0.5;                     
+R = 4;                        
+n_values = [1, 2, 4, 6, 8];  
+sim_time = 5000;             
+num_trials = 100;           
+lambda_E = 0.5;             
+
+
+AoI_inf = zeros(num_trials, length(n_values));   
+AoI_unit = zeros(num_trials, length(n_values));  
+
+
 for trial = 1:num_trials
     for ni = 1:length(n_values)
         n = n_values(ni);
+
+        %INFINITE BATTERY 
         AoI_users = ones(1, R);
         AoI_total = 0;
         t = 1;
@@ -23,6 +28,7 @@ for trial = 1:num_trials
             while any(received < n)
                 AoI_users = AoI_users + 1;
 
+               
                 if rand > q
                     received(received < n) = received(received < n) + 1;
                 end
@@ -36,18 +42,56 @@ for trial = 1:num_trials
                 AoI_users(:) = 1;
             end
         end
+        AoI_inf(trial, ni) = AoI_total / (sim_time * n);
 
-        AoI_packet(trial, ni) = AoI_total / (sim_time * n);
+        %  UNIT BATTERY
+        AoI_users = ones(1, R);
+        AoI_total = 0;
+        t = 1;
+        battery = 0;
+
+        while t <= sim_time
+            received = zeros(1, R);
+
+            while any(received < n)
+                AoI_users = AoI_users + 1;
+
+                if battery >= 1
+                    
+                    battery = battery - 1;
+                    if rand > q
+                        received(received < n) = received(received < n) + 1;
+                    end
+                else
+                    
+                    if rand < lambda_E
+                        battery = 1;
+                    end
+                end
+
+                AoI_total = AoI_total + mean(AoI_users);
+                t = t + 1;
+                if t > sim_time, break; end
+            end
+
+            if all(received >= n)
+                AoI_users(:) = 1;
+            end
+        end
+        AoI_unit(trial, ni) = AoI_total / (sim_time * n);
     end
 end
 
-%Ortalama
-mean_packet = mean(AoI_packet, 1);
 
-%Grafik
+mean_inf = mean(AoI_inf, 1);
+mean_unit = mean(AoI_unit, 1);
+
+
 figure;
-plot(n_values, mean_packet, '-o', 'LineWidth', 2);
+plot(n_values, mean_inf, '-o', 'LineWidth', 2); hold on;
+plot(n_values, mean_unit, '-s', 'LineWidth', 2);
 xlabel('n (Number of Packets in NC Block)');
 ylabel('Average AoI per Packet');
 title(['AoI per Packet vs n (q = ' num2str(q) ', R = ' num2str(R) ')']);
+legend('Infinite Battery', 'Unit Battery with EH');
 grid on;
